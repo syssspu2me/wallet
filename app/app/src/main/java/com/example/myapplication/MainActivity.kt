14b30464a60b3cfbcddf135897cfa14f721ac0d5
Vladimir <81909230+syssspu2me@users.noreply.github.com>
2023-03-23 22:13:14 +0300

    2424

--- app/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,40 +1,17 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import android.util.Log
+import kotlin.android.synthetic.main_
 
-private var reg = true
 class MainActivity : AppCompatActivity() {
-    // @SuppressLint("WrongViewCast")
-
     override fun onCreate(s: Bundle?) {
         super.onCreate(s)
         setContentView(R.layout.activity_main)
 
-
-        val getinput = findViewById<Button>(R.id.enter)
-        val editText = findViewById<EditText>(R.id.gettext)
-
-        getinput.setOnClickListener {
-            if (reg) {
-                val text = editText.text
-                if (text == null) {
-                    Toast.makeText(this, "null", Toast.LENGTH_SHORT).show()
-                } else {
-                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
-                    reg = false
-                }
-            } else {
-                Toast.makeText(this, "gg", Toast.LENGTH_SHORT).show()
-            }
-        }
-
+        button.setOnclickListener{ set_text("ok") }
+    }
+    fun set_text(str: String) {
+        costs.append(str)
     }
 }
